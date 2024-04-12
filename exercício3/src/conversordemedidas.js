89c69e1d0100013bc3626891c167fe48465ae28d
João <victorjhon779@gmail.com>
2024-04-11 22:32:49 -0300

    commit alteration

--- exercício3/src/conversordemedidas.js
+++ exercício3/src/conversordemedidas.js
@@ -6,26 +6,26 @@ let resultado;
 switch(unidadeDeMedida){
     case "milímetro" :
         resultado= valorAConverter * 1000
-        alert("o resultado é: " + resultado )
+        alert("o resultado é: " + resultado + "mm")
     break
     case "centímetro" :
          resultado= valorAConverter * 100
-        alert("o resultado é: " + resultado)
+        alert("o resultado é: " + resultado + "cm")
     break
     case "decímetro" :
         resultado= valorAConverter * 10
-        alert("o resultado é: " + resultado)
+        alert("o resultado é: " + resultado + "dc")
     break
     case "decâmetro" :
         resultado = valorAConverter / 10
-        alert("o resultado é: " + resultado)
+        alert("o resultado é: " + resultado + "dm")
     break
     case "hectômetro" :
         resultado= valorAConverter / 100
-        alert("o resultado é: " + resultado)
+        alert("o resultado é: " + resultado + "hc")
     case "quilometro" :
             resultado= valorAConverter / 1000
-            alert("o resultado é: " + resultado)
+            alert("o resultado é: " + resultado + "km")
     break
     default :
     alert("opção inválida")
